0fc098530ce6191ace336b6aec487080bbd3b467
Nick Jones <nick.jones@siliconmtn.com>
2022-12-21 15:01:30 -0700

    Initial commit adds StateMap class

---
+++ lib/src/main/java/nick/smt/training/exercises/Stack.java
@@ -0,0 +1,107 @@
+/**
+ * 
+ */
+package nick.smt.training.exercises;
+/*
+ * Vector requires java.util.* package
+ */
+import java.util.*;
+import java.util.Vector;
+
+
+/****************************************************************************
+ * <b>Title:</b> Stack.java
+ * <b>Project:</b> nicksiliconmtntrainings
+ * <b>Description:</b> Intro to Programming slideshow 2,
+ * This exercise is my own tinkering, but focuses on the use of constructors and method overloading to get me better practice.
+ * <b>Copyright:</b> Copyright (c) 2022
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author Nick Jones
+ * @version 3.0
+ * @since Dec 20 2022
+ * @updates:
+ ****************************************************************************/
+
+public class Stack {
+
+	/*
+	 * This is our main entry method for Java, this declares a new stack object, and
+	 * then prints to the console the result of the isEmpty method for the stack1
+	 * object. Because we use a constructor, we know that by default a stack object
+	 * has 10 items of the Vector class
+	 */
+	public static void main(String[] args) {
+
+		Stack stack1 = new Stack();
+
+		System.out.println("Items empty status: " + stack1.isEmpty());
+	}
+
+	
+	// Declare a private variable of the class Vector, called items	 
+
+	private Vector items;
+
+	/*
+	 * creates a constructor for the Stack class. Assigns a variable, items, to 10
+	 * instances of the Vector class
+	 */
+
+	public Stack() {
+
+		items = new Vector(10);
+	}
+
+	/*
+	 * Creates a method to add a object to the instances of item, called push
+	 * addElement is a method of the Vector class. Object is used because it returns
+	 * an instance of an object.
+	 */
+
+	public Object push(Object item) {
+
+		items.addElement(item);
+
+		return item;
+
+	}
+
+	/*
+	 * the following two methods demonstrate method overloading. Both methods have
+	 * the same name, but because I specify a different return type, I can reuse the
+	 * method name.
+	 */
+
+	static int plusMethod(int x, int y) {
+
+		return x + y;
+
+	}
+
+	/*
+	 * this method demonstrates method overloading, by using the same name as the
+	 * last method, but with a double return type, thus not breaking anything
+	 */
+
+	static double plusMethod(double x, double y) {
+
+		return x + y;
+
+	}
+
+	/*
+	 * this is a object method that acts on an instance of an items object, not the
+	 * Vector class.
+	 */
+
+	public boolean isEmpty() {
+
+		if (items.size() == 0)
+			return true;
+
+		else
+			return false;
+	}
+
+}
---
+++ lib/src/main/java/nick/smt/training/exercises/StateMap.java
@@ -0,0 +1,79 @@
+/**
+ * JDK 11
+ */
+package nick.smt.training.exercises;
+import java.util.*; // import the TreeMap class
+import java.util.HashMap; // import the HashMap class
+/****************************************************************************
+ * <b>Title:</b> StateMap.java
+ * <b>Project:</b> nicksiliconmtntrainings
+ * <b>Description:</b> Part 1 of the Collections Homework from PPT Intro to Programming 4 
+ * <b>Copyright:</b> Copyright (c) 2022
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author Nick Jones
+ * @version 3.0
+ * @since Dec 21 2022
+ * @updates:
+ ****************************************************************************/
+public class StateMap {
+	/**
+	 * @param args main runner for my StateMap class
+	 */
+	public static void main(String[] args) {
+		// Creates a HashMap object called States and assigns it to a 50 length Hashmap
+		// of key value pairs
+		final HashMap<Integer, String> States = new HashMap<Integer, String>();
+
+		// Calls addStates method on the States Hashmap
+
+		addStates(States);
+
+		// prints the values, and keys of States Hashmap
+		System.out.println(States.values());
+		System.out.println(States.keySet());
+
+		TreeMap orderedMap = orderMap(States);
+
+		iterateUsingEntrySet(States);
+
+		iterateUsingEntrySet(orderedMap);
+
+		orderedMap.descendingMap();
+
+	}
+	
+	/*
+	 * creates a class method that takes a map, and iterates through it, printing the keys and values
+	 */
+	public static void iterateUsingEntrySet(Map<Integer, String> map) {
+		for (Map.Entry<Integer, String> entry : map.entrySet()) {
+			System.out.println(entry.getKey() + ":" + entry.getValue());
+		}
+	}
+
+	/*
+	 * this function takes a hashmap and orders it
+	 */
+	public static TreeMap orderMap(HashMap x) {
+		TreeMap sortedMap = new TreeMap(x);
+		return sortedMap;
+	}
+
+	/**
+	 * @param x is a Hashmap that accepts a String, and String, set for a key value
+	 * 
+	 */
+	public static void addStates(HashMap<Integer, String> x) {
+		x.put(1, "Alabama");
+		x.put(2, "Alaska");
+		x.put(4, "Arizona");
+		x.put(6, "California");
+		x.put(8, "Colorado");
+		x.put(9, "Conneticut");
+		x.put(10, "Delaware");
+		x.put(15, "Hawaii");
+		x.put(48, "Texas");
+		x.put(39, "Ohio");
+	}
+}
