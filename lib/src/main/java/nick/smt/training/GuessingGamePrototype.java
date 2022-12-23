e8a2947282de057c227f400233aff3fb0a676e34
Nick Jones <nick.jones@siliconmtn.com>
2022-12-23 10:03:16 -0700

    enclosed a while loop for game prototype

--- lib/src/main/java/nick/smt/training/GuessingGamePrototype.java
+++ lib/src/main/java/nick/smt/training/GuessingGamePrototype.java
@@ -1,7 +1,12 @@
 /**
+JDK 11 and JRE 1.8
  * 
  */
 package nick.smt.training;
+
+import java.util.Random;
+
+import java.util.Scanner;
 
 /****************************************************************************
  * <b>Title:</b> GuessingGamePrototype.java
@@ -16,5 +21,42 @@ package nick.smt.training;
  * @updates:
  ****************************************************************************/
 public class GuessingGamePrototype {
+
+	public static void main(String[] args) {
+
+		Random rand = new Random();
+		int numberToGuess = rand.nextInt(1000);
+		int numberOfTries = 0;
+		Scanner input = new Scanner(System.in);
+		int guess;
+
+		boolean win = false;
+
+		while (win = false) {
+			System.out.println("Guess a number between 1 and 1000");
+			guess = input.nextInt();
+			if (guess == numberToGuess) {
+				win = true;
+
+				System.out.println("You got it! It took you " + numberOfTries + " to get it");
+			}
+
+			else if (guess < numberToGuess) {
+				System.out.println("Your guess is too low!");
+				System.out.println("Guess again!");
+				guess = input.nextInt();
+
+			}
+
+			else if (guess > numberToGuess) {
+				System.out.println("Your number was too high!");
+				System.out.println("Guess again!");
+				guess = input.nextInt();
+			}
+			numberOfTries++;
+
+		}
+
+	}
 
 }
